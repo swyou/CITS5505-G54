1733069b39d4ca7f72e6ff2ce582de6fbbe0dedb
S.W.You <songwenyou@gmail.com>
2025-05-07 16:50:18 +0800

    Fix: log out js moved to header.html to support logout everywhere.

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,33 @@ from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
 
+@main.route("/")
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.intro'))
+    return redirect(url_for('auth.login'))
+
+
+@main.route('/intro')
+@login_required
+def intro():
+    return render_template('intro.html', username=current_user.username)
+
+
+@main.route('/data')
+@login_required
+def data():
+    return render_template('data.html', username=current_user.username)
+
+@main.route('/share')
+@login_required
+def share():
+    return render_template('share.html', username=current_user.username)
+
+@main.route('/upload')
+@login_required
+def upload():
+    return render_template('upload.html', username=current_user.username)
 # 每 100g 平均热量
 AVG_KCAL_TABLE = {
     "meat": 200,
